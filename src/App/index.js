48f4c9b1ec4155ef272013c37ee2a008b6dd9d7f
Diego Portillo <djportillo@taxit.com.py>
2023-05-02 15:44:39 -0400

    Composición y colocación del estado en React 4/19

--- src/App/index.js
+++ src/App/index.js
@@ -3,13 +3,30 @@ import { TodoProvider } from "../TodoContext";
 import {AppUI} from './AppUI';
 import './App.css';
 
-function App() {
-
-  return (
-    <TodoProvider>
-        <AppUI/>
-    </TodoProvider>
+function App(){
+  return(
+    <React.Fragment>
+      <TodoHeader/>
+    </React.Fragment>
   );
 }
+function App(){
+  return;
+}
+function App(){
+  return;
+}
+function App(){
+  return;
+}
+
+// function App() {
+
+//   return (
+//     <TodoProvider>
+//         <AppUI/>
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
