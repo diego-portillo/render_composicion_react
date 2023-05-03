a10cff394fed74a7bbee40dc15e397936f64ed67
Diego Portillo <djportillo@taxit.com.py>
2023-05-03 13:43:02 -0400

    clase 4: Colocalcion estado

--- src/App/index.js
+++ src/App/index.js
@@ -2,22 +2,49 @@ import React from "react";
 import { TodoProvider } from "../TodoContext";
 import {AppUI} from './AppUI';
 import './App.css';
+// import { ToDoCounter } from "../ToDoCounter";
+// import { ToDoSearcher } from "../ToDoSearcher";
+// import { ToDoItem } from "../ToDoItem";
 
 function App(){
+  const [ state, setState] = React.useState('estado compartido');
   return(
-    <React.Fragment>
-      <TodoHeader/>
-    </React.Fragment>
+    
+      <React.Fragment>
+        <TodoHeader>
+          <ToDoCounter/>
+          <ToDoSearcher/>
+        </TodoHeader>
+        <TodoList>
+          <ToDoItem state={state}/>
+        </TodoList>
+      </React.Fragment>
+   
   );
 }
-function App(){
-  return;
+function TodoHeader({children}){
+  return(
+    <header>
+      {children}
+    </header>
+  );
 }
-function App(){
-  return;
+function TodoList({children}){
+  return(
+    <section className="TodoList-container">
+      {children}
+    </section>
+  );
 }
-function App(){
-  return;
+function ToDoCounter(){
+  // const [state, setState] = React.useContext(TodoProvider)
+  return <p>Contador</p>;
+}
+function ToDoSearcher(){
+  return <p>ToDoSearcher</p>;
+}
+function ToDoItem({state}){
+  return <p>ToDoItem: {state}</p>;
 }
 
 // function App() {
